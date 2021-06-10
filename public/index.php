75fb4dd126ef8cc51a6382a06898563a21bbd424
Dmitri BAJENOV <dbjenov@gmail.com>
2021-06-10 16:26:49 +0300

    html

---
+++ public/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>My CV in HTML</title>
+</head>
+<body>
+      
+      <h1> Bajenov Dmitrii</h1>
+
+
+      <ul>
+      <li>
+        <a href="">About</a> 
+      </li>
+
+      <li>
+        <a href="">Expirence</a> 
+      </li>
+      <li>
+        <a href="">Study</a>
+      </li>
+      <li>
+        <a href="">Contact</a>
+      </li>
+      </ul>
+    <?php echo "Today is " . date("Y/m/d H:i:s"); ?>
+      
+     
+</body>
+</html>
